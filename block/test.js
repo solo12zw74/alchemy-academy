7f8e95669c252608f2ecc3c73f380abe025a02ca
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-03-17 23:14:55 -0600

    Add more unit tests to Block class

--- block/test.js
+++ block/test.js
@@ -1,10 +1,22 @@
 const Block = require('./block');
 const assert = require('assert');
+const SHA256 = require("crypto-js/sha256");
 
 describe('Block', function() {
-    const newBlock = new Block();
-
     it('should have a hash property', function() {
+        const newBlock = new Block();
         assert(/^[0-9A-F]{64}$/i.test(newBlock.toHash()));
+    });
+
+    it('should store a random name', function() {
+        const randomName = require('faker').name.findName();
+        assert.equal(randomName, new Block(randomName).data)
+    });
+
+    it('should hash some random data', function() {
+        const randomEmail = require('faker').internet.email();
+        const myHash = SHA256(randomEmail).toString();
+        const yourHash = new Block(randomEmail).toHash().toString();
+        assert.equal(myHash, yourHash);
     });
 });
