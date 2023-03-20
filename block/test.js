b34dcd6e7b6be7b691ff7b31f1fdb157a18605e6
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-03-19 21:47:03 -0600

    Add unit tests for isValid function

--- block/test.js
+++ block/test.js
@@ -27,6 +27,12 @@ describe('Blockchain', function () {
         blockchain = new Blockchain();
     });
 
+    beforeEach(() => {
+        blockchain.addBlock(new Block("Dan"));
+        blockchain.addBlock(new Block("Peter"));
+        blockchain.addBlock(new Block("James"));
+    });
+
     it('should have a genesis block', function () {
         const genesisBlock = blockchain.chain[0];
         assert(genesisBlock, 'Could not find the genesis block!');
@@ -95,6 +101,30 @@ describe('Blockchain', function () {
                     assert.notEqual(initialBlock1Hash, newHash, "Expected changing the genesis blocks data to change the second blocks hash calculation!");
                 });
             });
+        });
+    });
+
+    it('should be considered valid', function () {
+        assert(blockchain.isValid());
+    });
+
+    describe('tampering with a previousHash', function () {
+        beforeEach(() => {
+            blockchain.chain[1].previousHash = SHA256("gibberish");
+        });
+
+        it('should not be considered valid', function () {
+            assert(!blockchain.isValid());
+        });
+    });
+
+    describe('tampering with data', function () {
+        beforeEach(() => {
+            blockchain.chain[0].data = "Something Else";
+        });
+
+        it('should not be considered valid', function () {
+            assert(!blockchain.isValid());
         });
     });
 })
