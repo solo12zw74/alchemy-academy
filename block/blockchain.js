6196d861f7cbd5835d941457e806e3240cb7af5c
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-03-19 22:03:18 -0600

    Implement the whole blockchain validation

--- block/blockchain.js
+++ block/blockchain.js
@@ -10,6 +10,16 @@ class Blockchain {
         block.previousHash = previousBlock.toHash()
         this.chain.push(block)
     }
+
+    isValid = function () {
+        let result = true;
+        for (let index = 1; index < this.chain.length; index++) {
+            let prevBlock = this.chain[index - 1]
+            let block = this.chain[index]
+            result &= prevBlock.toHash().toString() == block.previousHash
+        }
+        return result;
+    }
 }
 
 module.exports = Blockchain;
