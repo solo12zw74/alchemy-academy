3a81dfd4c849a0c56db98e7a4f0345a8c0e37377
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-03-24 22:10:56 -0600

    Add unit test checks input-output amount quality

--- txo/test.js
+++ txo/test.js
@@ -70,4 +70,41 @@ describe('Transaction', function () {
             assert(ex, "Did not throw an exception with a spent input TXO!");
         });
     });
+
+    describe('with sufficient UTXOs', () => {
+        const txo1 = new TXO(fromAddress, 5);
+        const txo2 = new TXO(fromAddress, 5);
+        const outputTXO1 = new TXO(toAddress, 10);
+        const tx = new Transaction([txo1, txo2], [outputTXO1]);
+
+        it('should execute without error', () => {
+            try {
+                tx.execute();
+            }
+            catch (ex) {
+                assert.fail(ex.message);
+                console.log(ex);
+            }
+        });
+    });
+
+    describe('with insufficient UTXOs', () => {
+        const txo1 = new TXO(fromAddress, 3);
+        const txo2 = new TXO(fromAddress, 3);
+        const txo3 = new TXO(fromAddress, 3);
+        const outputTXO1 = new TXO(toAddress, 10);
+
+        const tx = new Transaction([txo1, txo2, txo3], [outputTXO1]);
+
+        it('should throw an error on execute', () => {
+            let ex;
+            try {
+                tx.execute();
+            }
+            catch (_ex) {
+                ex = _ex;
+            }
+            assert(ex, "Did not throw an exception for a lack of UTXO funds!");
+        });
+    });
 });
