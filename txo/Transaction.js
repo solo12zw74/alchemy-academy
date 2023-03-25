4b793d81c27fe9d24f92037ccc124336fe08e991
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-03-24 22:21:15 -0600

    Implement amounts check

--- txo/Transaction.js
+++ txo/Transaction.js
@@ -1,11 +1,22 @@
 class Transaction {
+    #sumReducer(accumulator, txo) {
+        return accumulator + txo.amount
+    }
     constructor(inputUTXOs, outputUTXOs) {
         this.inputs = inputUTXOs
         this.outputs = outputUTXOs
     }
+
     execute() {
         if (this.inputs.some(x => x.spent)) {
             throw new Error("Spent transactions found")
+        }
+
+        const income = this.inputs.reduce(this.#sumReducer, 0);
+        const outcome = this.outputs.reduce(this.#sumReducer, 0);
+        console.log(`income: ${income}`)
+        if (income < outcome) {
+            throw new Error("Insufficient funds")
         }
     }
 }
