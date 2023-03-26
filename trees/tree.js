21731b4664e1fcc650f54ef449ac46428a9ca75a
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-03-25 21:23:22 -0600

    Implement recursive tree building

--- trees/tree.js
+++ trees/tree.js
@@ -3,19 +3,34 @@ class Tree {
         this.root = null
     }
 
-    addNode = function(node){
-        if (this.root == null){
+    addNode = function (node) {
+        if (this.root == null) {
             this.root = node
-        }
-
-        if (node.data < this.root.data) {
-            this.root.left = node
             return
         }
 
-        if(node.data > this.root.data) {
-            this.root.right = node
+        addNodeRecursively(this.root, node)
+    }
+}
+
+function addNodeRecursively(parentNode, childNode) {
+
+    if (childNode.data < parentNode.data) {
+        if (!!parentNode.left) {
+            addNodeRecursively(parentNode.left, childNode);
+        } else {
+            parentNode.left = childNode;
         }
+        return;
+    }
+
+    if (childNode.data > parentNode.data) {
+        if (!!parentNode.right) {
+            addNodeRecursively(parentNode.right, childNode);
+        } else {
+            parentNode.right = childNode;
+        }
+        return;
     }
 }
 
