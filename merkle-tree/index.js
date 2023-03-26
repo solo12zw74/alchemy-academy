767bd7f8d0ba96b7affcb6177a597cdab10dffd3
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-03-26 14:14:21 -0600

    Implement recursive Merkle-tree hash building

--- merkle-tree/index.js
+++ merkle-tree/index.js
@@ -4,7 +4,25 @@ class MerkleTree {
         this.concat = concat
     }
     getRoot() {
-       return this.concat(this.leaves[0], this.leaves[1])
+        if (this.leaves.length == 1) {
+            return this.leaves[0]
+        }
+
+        return this.getRootRecursively(this.leaves)
+    }
+
+    getRootRecursively(arr) {
+
+        if (arr.length == 2) {
+            return this.concat(arr[0], arr[1])
+        }
+        const { left, right } = this.split(arr)
+        return this.concat(this.getRootRecursively(left), this.getRootRecursively(right))
+    }
+
+    split(arr) {
+        const length = arr.length
+        return { left: arr.splice(0, length / 2), right: arr }
     }
 }
 
