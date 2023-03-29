d74bcd65503d7abf275837bccbfafd49d50b4f49
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-03-28 22:17:18 -0600

    Implement basic trie build

--- tries/trie.js
+++ tries/trie.js
@@ -4,6 +4,19 @@ class Trie {
     constructor() {
         this.root = new TrieNode(null)
     }
+
+    insert = function (word) {
+        let node = this.root;
+
+        for (let i = 0; i < word.length; i++) {
+            node.children[word[i]] = new TrieNode(word[i]);
+            node = node.children[word[i]];
+
+            if (i == word.length - 1) {
+                node.isWord = true;
+            }
+        }
+    }
 }
 
 module.exports = Trie;
